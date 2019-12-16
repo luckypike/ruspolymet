278db4eb15f9290adfa84c7f74ba014dab08d42b
Andy Braga <ab@luckypike.com>
2019-12-16 19:54:46 +0300

    Наброски

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Header from './Header'
+
+// import logo from './logo.svg';
+import styles from './App.module.css'
+
+export default function App () {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Header.module.css'
+
+export default function Header () {
+  return (
+    <div>
+      <div className={styles.container}>
+        ZZZ
+      </div>
+    </div>
+  )
+}
