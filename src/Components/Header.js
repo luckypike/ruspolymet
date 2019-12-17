d0840bb5dde2e56b8db4227a4cf8cb088b100530
Andy Braga <ab@luckypike.com>
2019-12-17 19:23:12 +0300

    Добавлен постер

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,12 +3,13 @@ import React from 'react'
 import styles from './Header.module.css'
 
 import Intro from './Video/Intro.mp4'
+import Poster from './Video/Poster.jpg'
 
 export default function Header () {
   return (
     <div>
       <div className={styles.container}>
-        <video playsInline autoPlay loop muted>
+        <video playsInline autoPlay loop muted poster={Poster}>
           <source src={Intro} type="video/mp4" />
         </video>
 
