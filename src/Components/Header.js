bedebb4637af9af5b19e105a5c5f5301c361be49
Maxim <max.spiridonov@gmail.com>
2019-12-17 14:25:54 +0300

    Добавлено видео в header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 import styles from './Header.module.css'
 
+import Intro from './Video/Intro.mp4'
+
 export default function Header () {
   return (
     <div>
       <div className={styles.container}>
+        <video playsInline autoPlay loop muted>
+          <source src={Intro} type="video/mp4" />
+        </video>
+
         <div className={styles.text}>
           <h1>
             Вместе — сильнее!
